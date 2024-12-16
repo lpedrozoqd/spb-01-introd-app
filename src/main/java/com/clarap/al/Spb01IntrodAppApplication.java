7acf440ba813564f64584c77a358b5c57da60a53
lpedrozoqd <leonel.pedrozo.quiroz.d@gmail.com>
2024-12-15 19:33:44 -0500

    Config. incial

---
+++ src/main/java/com/clarap/al/Spb01IntrodAppApplication.java
@@ -0,0 +1,13 @@
+package com.clarap.al;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spb01IntrodAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spb01IntrodAppApplication.class, args);
+	}
+
+}
